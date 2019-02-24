efd6ae99669ac76693184598edc046d4842e743e
Roy Chin <info@roychin.nl>
2019-02-24 21:00:45 +0100

    Making sure all usernames are unique.

--- server/models/user.js
+++ server/models/user.js
@@ -3,11 +3,27 @@
  */
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        realName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        roles: DataTypes.JSON,
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        realName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        roles: {
+            type: DataTypes.JSON,
+            allowNull: false
+        },
         profile: DataTypes.JSON
     });
 
